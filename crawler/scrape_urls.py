fb3ef6c0aedb980aa9db088c8994b62b54b2e6bd
Michal Jan Warecki <mwarecki1@gmail.com>
2022-01-12 20:16:50 +0100

    URL scraping

---
+++ crawler/scrape_urls.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup as Soup
+from urllib.request import urlopen
+
+url = "https://en.wiktionary.org/w/index.php?title=Special:AllPages&from=Old+Dutch%2Fbar&namespace=118"
+with urlopen(url) as fh:
+    html = fh.read()
+
+soup = Soup(html, "lxml")
+hrefs = [y['href'] for y in
+      [x for x in soup.find_all('a') if x.has_attr('href')]
+    ]
+for href in hrefs:
+  print(href)
