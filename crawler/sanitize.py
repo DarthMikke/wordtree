cbdd2512979bcf10211986a025cedba313f4af6f
Michal Jan Warecki <mwarecki1@gmail.com>
2022-01-12 17:43:39 +0100

    Language sanitizing script.

---
+++ crawler/sanitize.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+# Crawler sanitizing scripts
+
+import os
+import sys
+import django
+
+sys.path.append(os.getcwd())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{os.path.split(os.getcwd())[-1]}.settings')
+print(os.getcwd())
+django.setup()
+
+from wordtree.models import Language
+
+if __name__ == "__main__":
+    endings = ["[edit]", ": "]
+
+    for x in endings:
+        langs = Language.objects.filter(name__endswith=x)
+        for lang in langs:
+            old = lang.name
+            new = lang.name[:len(x)]
+            print(f"{old} -> {new}")
+            lang.name = new
+
+        if len(langs) > 0:
+            ok = input("Are these changes OK? [y/n]")
+            if ok == "y":
+                for lang in langs:
+                    lang.save()
+            print("Saved.")
+        else:
+            print(f"No changes to make at `{x}`.")
