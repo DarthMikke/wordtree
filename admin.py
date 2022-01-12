5c15c6120c4f512c24cba162b803d9a9e74e72bd
Michal Jan Warecki <mwarecki1@gmail.com>
2022-01-12 14:34:54 +0100

    Source link in word admin page.

--- admin.py
+++ admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html  # Thank you https://stackoverflow.com/questions/1949248/how-to-add-clickable-links-to-a-field-in-django-admin
 from .models import Language, Word
 
 
@@ -14,6 +15,14 @@ def mark_approved(modeladmin, request, queryset):
 class WordAdmin(admin.ModelAdmin):
 	list_display = ("romanize", "language", "approved")
 	actions = [mark_approved]
+	readonly_fields = ['source_link']
+	fieldsets = (
+		(None, {'fields': ('text', 'romanized', 'language', 'parent', 'reconstructed', )}),
+		(None, {'fields': ('approved', ('source', 'source_link'))})
+	)
+
+	def source_link(self, obj):
+		return format_html('<a href="{url}">Link</a>', url=obj.source)
 
 
 # Register your models here.
