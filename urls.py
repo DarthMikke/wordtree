f2810092bd768c1d0a5e914dd4c8c85287027c13
Michal Jan Warecki <mwarecki1@gmail.com>
2022-01-16 12:56:43 +0100

    New path to index.

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
 	path('image/<str:word_pks>.png', views.image),
 	path('index.html', views.index),
+	path('', views.index),
 	path('tree.html', views.tree, name='word_tree'),
 	path('api/autofill', views.search),
 ]
