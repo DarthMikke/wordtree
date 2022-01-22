cbce0a8844deac5a028c3c31d0b2d481048b9e36
Michal Jan Warecki <mwarecki1@gmail.com>
2022-01-22 19:57:04 +0100

    Better URL scraping

--- crawler/scrape_urls.py
+++ crawler/scrape_urls.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup as Soup
 from urllib.request import urlopen
 
-url = "https://en.wiktionary.org/w/index.php?title=Special:AllPages&from=Old+Dutch%2Fbar&namespace=118"
+# Original URL: "https://en.wiktionary.org/w/index.php?title=Special:AllPages&from=Old+Dutch%2Fbar&namespace=118"
+url = input("url: ")
 with urlopen(url) as fh:
     html = fh.read()
 
 soup = Soup(html, "lxml")
-hrefs = [y['href'] for y in
-      [x for x in soup.find_all('a') if x.has_attr('href')]
+hrefs = [
+    y['href'] for y in [
+        x for x in soup.find_all('a') if x.has_attr('href')
     ]
+]
 for href in hrefs:
-  print(href)
+    print(href)
