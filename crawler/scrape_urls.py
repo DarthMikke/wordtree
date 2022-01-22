fda75716b0a973432d262425cd368f38cec67cc3
Michal Jan Warecki <mwarecki1@gmail.com>
2022-01-22 19:59:26 +0100

    URL scraping with CLI args.

--- crawler/scrape_urls.py
+++ crawler/scrape_urls.py
@@ -1,8 +1,13 @@
+import sys
 from bs4 import BeautifulSoup as Soup
 from urllib.request import urlopen
 
 # Original URL: "https://en.wiktionary.org/w/index.php?title=Special:AllPages&from=Old+Dutch%2Fbar&namespace=118"
-url = input("url: ")
+try:
+    url = sys.argv[-1] if len(sys.argv) < 2 else input("url: ")
+except KeyboardInterrupt:
+    exit()
+
 with urlopen(url) as fh:
     html = fh.read()
 
