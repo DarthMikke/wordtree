0f0cd2e45a64f96ed824dd5a21cc05684c8093cb
Michal Jan Warecki <mwarecki1@gmail.com>
2023-10-27 22:27:36 +0200

    Change Django app name from 'api' to 'wordtree'

--- wordtree/urls.py
+++ wordtree/urls.py
@@ -1,22 +1,10 @@
-"""wordtree URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+import wordtree.views as views
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('/', include('api.urls')),
+	path('image/<str:word_pks>.png', views.image),
+	path('index.html', views.index),
+	path('', views.index),
+	path('tree.html', views.tree, name='word_tree'),
+	path('api/autofill', views.search),
 ]
