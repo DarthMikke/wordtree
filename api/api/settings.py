aa611530ffcb35e53d7b90e0c1cc33aecb7639e5
Michal Jan Warecki <mwarecki1@gmail.com>
2023-11-13 18:11:43 +0100

    Settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)ug*t3ppw+w#3(615l-zxlw=3(s179)w)guav4o3u4$h#gir9d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wordtree.millim.test', 'wordtree.millim.no']
 
 
 # Application definition
